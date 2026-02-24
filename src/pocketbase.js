aa9419e526a9005d16cc59b176dd35a60cba16ba
Дмитрий <dv-d3h@outlook.com>
2026-02-24 14:50:09 +0300

    Заменили константу URL сервера на переменную окружения

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('https://pb.dev.zavidovo.school');
+const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 pb.autoCancellation(false);
 export default pb;
